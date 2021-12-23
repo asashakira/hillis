0424dc050b786d22c0b6d64da0bdfa5c9c9397f4
akira <akira340@users.noreply.github.com>
2021-12-23 18:17:02 +0900

    rewrote entire code

--- hillis/sol.cpp
+++ hillis/sol.cpp
@@ -34,182 +34,219 @@ void print2d(vector<vector<T>> v) {
   }
 }
 
-#define N 4
-#define M 4
-#define POP_SIZE 10
-#define MAX_GEN 10
+class SortingNetwork {
+private:
+  vector<pair<int,int>> c1, c2, compares;
+  float fitness = 0;
+  int input_size;
 
-class Kotai {
-  public:
-  vector<vector<int>> c1, c2, c;
-  int fitness = 0;
-
-  Kotai() : c1(M, vector<int>(2)), c2(M, vector<int>(2)) {
-    for (int i = 0; i < M; i++) {
-      c1[i][0] = random(0, N);
-      c1[i][1] = random(0, N);
-      c2[i][0] = random(0, N);
-      c2[i][1] = random(0, N);
-    }
-    merge();
-  }
-
-  void merge() {
-    for (int i = 0; i < M; i++) {
-      if (c1[i][0] == c2[i][0] and c1[i][1] == c2[i][1]) {
-        c.push_back(c1[i]);
+  void Merge() {
+    compares.clear();
+    for (int i = 0; i < (int)c1.size(); i++) {
+      if (c1[i].first == c2[i].first and c1[i].second == c2[i].second) {
+        compares.push_back(c1[i]);
       } else {
-        c.push_back(c1[i]);
-        c.push_back(c2[i]);
+        compares.push_back(c1[i]);
+        compares.push_back(c2[i]);
       }
     }
   }
-
-  Kotai cross(Kotai b, int cut) {
-    Kotai nk; // new kotai
-    for (int i = 0; i < cut; i++) {
-      nk.c1[i] = c1[i];
-      nk.c2[i] = c2[i];
+public:
+  SortingNetwork() { }
+  SortingNetwork(int size, int inputsize) : c1(size), c2(size) {
+    input_size = inputsize;
+    // create random network
+    // c1
+    for (auto &[a, b] : c1) {
+      do {
+        a = random(0, inputsize);
+        b = random(0, inputsize);
+      } while (a == b); // re-roll until a != b
+      if (a > b) swap(a, b); // a must be less than b
     }
-    for (int i = cut; i < M; i++) {
-      nk.c1[i] = b.c1[i];
-      nk.c2[i] = b.c2[i];
+    // c2
+    for (auto &[a, b] : c2) {
+      do {
+        a = random(0, inputsize);
+        b = random(0, inputsize);
+      } while (a == b); // re-roll until a != b
+      if (a > b) swap(a, b); // a must be less than b
     }
-    merge();
-    return nk;
   }
+  SortingNetwork(vector<pair<int,int>> const &c) { compares = c; }
 
-  void calc_fitness() {
-    // making test case
-    map<vector<int>,int> mp;
+  void EvaluateFitness() {
+    fitness = 0;
+    map<vector<int>,int> mp; // to not test duplicates
+    int max_tests = 10;
     int tests = 0;
-    while (tests < 1) {
-      vector<int> a(N);
+    while (tests < max_tests) {
+      vector<int> v(input_size);
       int zeros = 0;
-      for (int i = 0; i < N; i++) {
-        a[i] = random(0, 2);
-        if (a[i] == 0) zeros++;
+      for (auto &x : v) {
+        x = random(0, 2);
+        zeros += x == 0;
       }
-      if (mp.find(a) != mp.end()) continue;
+      if (mp.find(v) != mp.end()) continue;
       tests++;
-
-      // apply sorting network
-      for (int i = 0; i < (int)c.size(); i++) {
-        if (a[c[i][0]] > a[c[i][1]])
-          swap(a[c[i][0]], a[c[i][1]]);
-      }
-
-      // calc fit
-      for (int i = 0; i < N; i++) {
+      Sort(v);
+      // calculating fitness
+      for (auto x : v) {
         if (zeros) {
-          fitness += (a[i] == 0 ? 1 : 0);
+          fitness += x == 0 ? 1 : 0;
           zeros--;
         } else {
-          fitness += (a[i] == 1 ? 1 : 0);
+          fitness += x == 1 ? 1 : 0;
         }
       }
     }
   }
 
-  int size() { return M; }
-  void print() {
-    for (int i = 0; i < (int)c.size(); i++) {
-      for (int j = 0; j < (int)c[i].size(); j++) {
-        cout << c[i][j] << ' ';
-      }
+  void Cross(SortingNetwork &b, int cut) {
+    for (int i = 0; i < cut; i++) {
+      swap(c1[i], b.c1[i]);
+      swap(c2[i], b.c2[i]);
+    }
+  }
+
+  void Sort(vector<int> &v) {
+    Merge();
+    for (auto [a, b] : compares)
+      if (v[a] > v[b]) swap(v[a], v[b]);
+  }
+
+  float Fitness() { return fitness; }
+  void Printc1() { for (auto [a, b] : c1) cout << a << ' ' << b << '\n'; }
+  void Printc2() { for (auto [a, b] : c2) cout << a << ' ' << b << '\n'; }
+  void Print() { Merge(); for (auto [a, b] : compares) cout << a << ' ' << b << '\n'; }
+};
+
+class GeneticAlgorithm {
+private:
+  vector<SortingNetwork> population;
+  SortingNetwork bestNetwork;
+  vector<pair<double,double>> roulette;
+  int population_size;
+  int crossover_rate;
+  int mutation_rate;
+  int best_individual;
+  float best_score;
+  int test_input_size;
+  int compare_size;
+public:
+  GeneticAlgorithm(int popsize, int crossover, int mutation, int inputsize, int comparesize, int testinputsize) {
+    population_size = popsize;
+    crossover_rate = crossover;
+    mutation_rate = mutation;
+    compare_size = comparesize;
+    // making population
+    for (int i = 0; i < popsize; i++)
+      population.push_back(SortingNetwork(comparesize, inputsize));
+  }
+
+  void Evaluate() {
+    // evaluate fitness of each network
+    for (auto &network : population)
+      network.EvaluateFitness();
+    // sort population by fitness value
+    sort(population.rbegin(), population.rend(), [](auto &left, auto &right) {
+      return left.Fitness() < right.Fitness();
+    });
+  }
+
+  int Selection() {  // roulette selection
+    double r = random(0.0, 100.0);
+    int ok = 0, ng = population_size;
+    while (abs(ok-ng) > 1) {
+      int mid = (ok+ng) / 2;
+      auto a = roulette[mid].first;
+      if (r < a) ng = mid;
+      else ok = mid;
+    }
+    return ok;
+  }
+
+  void Crossover() {
+    // sum of fitness
+    double sum = 0;
+    for (auto individual : population) sum += individual.Fitness();
+    // make roulette
+    roulette.assign(population_size, {0, 0});
+    roulette[0].first = 0;
+    roulette.back().second = 100.0;
+    for (int i = 0; i+1 < population_size; i++) {
+      double p = population[i].Fitness() / sum * 100;
+      p += roulette[i].first;
+      roulette[i].second = p;
+      roulette[i+1].first = p;
+    }
+    // new sedai
+    vector<SortingNetwork> new_population(population_size);
+    int i = 0;
+    while (i < crossover_rate) {
+      // select two parents
+      int a = Selection(), b = Selection();
+      int cut = random(1, compare_size-1);
+      population[a].Cross(population[b], cut);
+      new_population[i++] = population[a];
+      new_population[i++] = population[b];
+    }
+    for (int j = 0; i < population_size; i++, j++)
+      new_population[i] = population[j];
+    swap(population, new_population);
+  }
+
+  void Mutation() {
+  }
+
+  SortingNetwork GetBestNetwork() {
+    Evaluate();
+    return population[0];
+  }
+
+  void Print() {
+    for (auto x : population) {
+      cout << x.Fitness() << '\n';
+      x.Print();
       cout << '\n';
     }
   }
 };
 
-int selection(vector<pair<double,double>> roulette) {
-  double r = random(0.0, 100.0);
-  int ok = 0, ng = POP_SIZE;
-  while (abs(ok-ng) > 1) {
-    int mid = (ok+ng) / 2;
-    auto a = roulette[mid].first;
-    if (r < a) ng = mid;
-    else ok = mid;
-  }
-  return ok;
-}
-
-vector<Kotai> crossover(vector<Kotai> s) {
-  // sum of fitness
-  double sum = 0;
-  for (auto k : s) sum += k.fitness;
-  // slice pie
-  vector<pair<double,double>> roulette(POP_SIZE);
-  roulette[0].first = 0;
-  roulette.back().second = 100.0;
-  for (int i = 0; i+1 < POP_SIZE; i++) {
-    double p = s[i].fitness / sum * 100;
-    p += roulette[i].first;
-    roulette[i].second = p;
-    roulette[i+1].first = p;
-  }
-
-  vector<Kotai> ns(POP_SIZE);
-  int i = 0;
-  while (i < POP_SIZE/2) {
-    int a = selection(roulette);
-    int b = selection(roulette);
-    int cut = random(1, s[a].size());
-    a = 0; b = 1;
-    cut = 1;
-    ns[i++] = s[a].cross(s[b], cut);
-    ns[i++] = s[b].cross(s[a], cut);
-  }
-  int j = 0;
-  while (i < POP_SIZE) {
-    ns[i] = s[j++];
-  }
-  return ns;
-}
-
-void sortingNetwork(vector<vector<int>> c) {
-  // making test case
-  vector<int> a(N);
+void Test(SortingNetwork &sn, int n) {
+  vector<int> a(n);
   iota(a.begin(), a.end(), 0);
-  // shuffle
-  for (int i = 0; i < N; i++) {
-    int r = random(0, N);
+  for (int i = 0; i < n; i++) {
+    int r = random(0, n);
     swap(a[i], a[r]);
   }
-
-  // apply sorting network
-  for (int i = 0; i < (int)c.size(); i++) {
-    if (a[c[i][0]] > a[c[i][1]])
-      swap(a[c[i][0]], a[c[i][1]]);
+  auto b = a;
+  sort(b.begin(), b.end());
+  sn.Sort(a);
+  for (int i = 0; i < n; i++) {
+    cout << b[i] << ' ' << a[i] << '\n';
   }
-  for (auto x : a) cout << x << '\n';
 }
 
 signed main() {
-  // sedai
-  vector<Kotai> sedai(POP_SIZE);
+  const int popsize = 10;
+  const int crossover = popsize/2;
+  const int mutation = 10;
+  const int inputsize = 4;
+  const int comparesize = inputsize;
+  const int testinputsize = inputsize*50;
+  const int max_iterations = 0;
 
-  // MAX_GEN回繰り返す
-  for (int g = 0; g < MAX_GEN; g++) {
-
-    // 個体の評価を計算
-    for (int i = 0; i < POP_SIZE; i++)
-      sedai[i].calc_fitness();
-
-    // 評価が高い順にソート
-    sort(sedai.rbegin(), sedai.rend(), [](auto &left, auto &right) {
-      return left.fitness < right.fitness;
-    });
-
-    // 新世代を交叉・突然変異で作成
-    auto new_sedai = crossover(sedai);
-    //sedai = mutation(new_sedai);
-    sedai = new_sedai;
-
-    cout << '\n';
-    for (int i = 0; i < POP_SIZE; i++)
-      cout << sedai[i].fitness << '\n';
+  GeneticAlgorithm ga(popsize, crossover, mutation, inputsize, comparesize, testinputsize);
+  ga.Evaluate();
+  for (int it = 0; it < max_iterations; it++) {
+    ga.Crossover();
+    ga.Mutation();
+    ga.Evaluate();
   }
-
-  return 0;
+  // ga.Print();
+  auto sn = ga.GetBestNetwork();
+  sn.Print();
+  cout << '\n';
+  Test(sn, inputsize);
 }
